ed2253fd62296e86957c1e2723ed1914f39fc5eb
Irv Kalb <Irv@furrypants.com>
2018-12-15 12:41:36 -0800

    Preparing for 0.9.4 release
    Modified configuration files for 0.9.4 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyghelpers',
-    version='0.9.3',
+    version='0.9.4',
     author='Irv Kalb',
     author_email='Irv@furrypants.com',
     description='Classes and functions for use with Pygame',
