be4c6e405783dc9f0570c88e50fdde162040ee16
Irv Kalb <Irv@furrypants.com>
2018-12-24 10:22:50 -0800

    Updated to version 0.9.5
    Updated to version 0.9.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyghelpers',
-    version='0.9.4',
+    version='0.9.5',
     author='Irv Kalb',
     author_email='Irv@furrypants.com',
     description='Classes and functions for use with Pygame',
