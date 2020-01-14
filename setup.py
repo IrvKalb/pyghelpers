162e3b8ca048d968ae1bb96d272b962faa0e9de3
Irv Kalb <Irv@furrypants.com>
2020-01-13 20:24:29 -0800

    Release 1.0
    Release 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyghelpers',
-    version='0.9.9',
+    version='1.0.0',
     author='Irv Kalb',
     author_email='Irv@furrypants.com',
     description='Classes and functions for use with Pygame',
